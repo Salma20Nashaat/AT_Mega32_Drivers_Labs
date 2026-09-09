4769ddc670d6c10c3a1921c75cb40023f5cc9f14
Salma Nashaat Mohammed <ismailshaza7@gmail.com>
2026-09-09 04:34:50 +0300

    Update main_lab3.c

--- Applications/labs/TIMER1&0/main_lab3.c
+++ Applications/labs/TIMER1&0/main_lab3.c
@@ -1,45 +1,18 @@
 #include "STD_TYPES.h"
 #include "BIT_MATH.h"
 #include "DIO_int.h"
-#include "TIMER_int.h"
-#include "ADC_H.h"
-#include "DIO_private.h"
-#include <avr/interrupt.h>
-
-volatile u8 counter = 0;
+#include "TIMER1_private.h"
+#include "TIMER1_int.h"
+#include <util/delay.h>
 
 int main(void)
 {
-	u16 adc_value =0;
-
-	
-	DIO_voidSetPinDirection(PORTD, PIN0,OUTPUT);
-	DIO_voidSetPinDirection(PORTD, PIN1,OUTPUT);
-	DIO_voidSetPinDirection(PORTD, PIN2,OUTPUT);
-	
-	DIO_voidSetPinDirection(PORTA, PIN0,INPUT);
-
-	voidTimerInit();
-	ADC_Init();
-	set_bit(SREG,7);
-
-	
+	DIO_voidSetPinDirection(PORTD, PIN5, OUTPUT);
+	voidTimer1Init();
 
 	while(1)
-	{    adc_value = ADC_Read(0);
-		OCR0 = (u8)(adc_value / 4);
-	}
-}
-
-ISR(TIMER0_COMP_vect)
-{
-	counter++;
-	
-	if (counter >= 50)
 	{
-		toggle_bit(DPORTD,0);
-		toggle_bit(DPORTD,1);
-		toggle_bit(DPORTD,1);
-		counter = 0;
+		Timer1ServoAngel(90);
+		
 	}
 }
